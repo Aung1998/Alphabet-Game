907a2708a7d73c86111c6b6dfe31bf60f3903da9
ARAA <40233986+afsalahamed07@users.noreply.github.com>
2021-07-20 14:24:39 +0530

    update stat functon add

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ function displayTimer() {
 function checkAlphabet(str){
     return str[0] == 'a' && alphabet.includes(str);
 }
+
+
+// global var to score score and health
+let score = 0;
+let health = 10;
+
+// grab dom element for score and health
+let dom_score = document.querySelector("elementOfScore")
+let dom_helaht = document.querySelector("elementofHealth")
+
+//  functon update helath and score
+function updateStat() {
+    if (checkAlphabet(str)) { // right answer
+        score++;
+        dom_score.textContent = score; // update score on dom
+    }else{ // wrong answer
+        health--;
+        dom_helaht.textContent = health; // update health on dom
+    }
+} 
